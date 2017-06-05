3754b638daf59ba08d0c1f39b229a4ec37c74534
Inon Baguio <inon.baguio@yahoo.com>
2017-06-05 16:40:28 +0800

    used make method

--- tests/Feature/ParticipateInForumTest.php
+++ tests/Feature/ParticipateInForumTest.php
@@ -12,13 +12,21 @@ class ParticipateInForumTest extends TestCase
     use DatabaseMigrations;
 
     /** @test */
+    public function unauthenticated_users_may_not_add_replies()
+    {
+        $this->expectException('Illuminate\Auth\AuthenticationException');
+
+        $this->post('/threads/1/replies', []);
+    }
+
+    /** @test */
     public function an_authenticated_user_may_participate_in_forum_threads()
     {
         $this->be($user = factory('App\User')->create());
 
         $thread = factory('App\Thread')->create();
 
-        $reply = factory('App\Reply')->create();
+        $reply = factory('App\Reply')->make();
 
         $this->post($thread->path() . '/replies', $reply->toArray());
 
