2d130e5f4d19d88cd927aece67e37d388352a92d
Inon Baguio <inon.baguio@yahoo.com>
2017-06-28 16:31:36 +0800

    #13 used view composer for channels

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -15,7 +15,7 @@
                             <label for="channel_id">Choose a Channel: </label>
                             <select name='channel_id' id='channel_id' class='form-control' required>
                                 <option value=''>Choose One</option>
-                                @foreach(App\Channel::all() as $channel)
+                                @foreach($channels as $channel)
                                     <option value='{{ $channel->id }}' {{ old('channel_id') === $channel->id ? 'selected' : '' }}>
                                         {{ $channel->name }}
                                     </option>
